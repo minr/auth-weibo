1812b4e47a7e450bf9dbb5e19d6c84fb7759e387
内裤叔叔 <min@blued.com>
2021-10-14 14:08:49 +0800

    Support for flarum 1. X

--- extend.php
+++ extend.php
@@ -1,10 +1,23 @@
 <?php
+/*
+ * This file is part of minr/flarum-ext-auth-weibo.
+ *
+ * Copyright (c) 2021 Minr.
+ *
+ * For the full copyright and license information, please view the LICENSE.md
+ * file that was distributed with this source code.
+ */
+
+use Flarum\Api\Serializer\UserSerializer;
 use Flarum\Extend;
+use FoF\Components\Extend\AddFofComponents;
 
 return [
+    new AddFofComponents(),
+
     (new Extend\Frontend('forum'))
         ->js(__DIR__.'/js/dist/forum.js')
-        ->css(__DIR__.'/less/forum.less'),
+        ->css(__DIR__ . '/less/forum.less'),
 
     (new Extend\Frontend('admin'))
         ->js(__DIR__.'/js/dist/admin.js'),
